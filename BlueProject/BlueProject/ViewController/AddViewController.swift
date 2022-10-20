a5433caf32f9055af8cb8862e4ea10c98e971904
june <eptm917@gmail.com>
2022-10-20 17:07:06 +0900

    addViewController 화면 구성

--- BlueProject/BlueProject/ViewController/AddViewController.swift
+++ BlueProject/BlueProject/ViewController/AddViewController.swift
@@ -9,13 +9,30 @@ import UIKit
 
 class AddViewController: UIViewController {
     
+    @IBOutlet var AddButton: UIButton!
+    @IBOutlet var field2: UITextField!
     @IBOutlet var field: UITextField!
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         field.returnKeyType = .done
         field.borderStyle = .bezel
         field.textColor = .black
-        field.placeholder = "입력하세요"
+        field.placeholder = "아이디입력하세요"
+        field2.returnKeyType = .done
+        field2.borderStyle = .bezel
+        field2.textColor = .black
+        field2.placeholder = "닉네임입력하세요"
+        AddButton.tintColor = .white
+        
+        
+        
     }
-
+    
+    @IBAction func cameraButtonAc(_ sender: Any) {
+        
+    }
+    
 }
